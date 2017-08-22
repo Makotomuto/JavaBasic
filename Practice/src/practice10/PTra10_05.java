09b257ce7a1373a61ac2b9f4e728dd6c0d9a9dee
DESKTOP-D7L303E\likeit_student <hey.shoei.doesn.t.it.do@gmail.com>
2017-08-22 18:59:39 +0900

    PTra11_01まで完了。

--- Practice/src/practice10/PTra10_05.java
+++ Practice/src/practice10/PTra10_05.java
@@ -23,20 +23,21 @@ public class PTra10_05 {
 	 * 			ガソリンが負の数になった場合（もう進めない）は-1を返します。
 	 */
 
+
 	public static void main(String[] args) {
 
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
-
+		Car car = new Car();
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
-
+		car.serialNo = 10000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
-
+		car.color = "Red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
-
+		car.gasoline = 50;
 
 		// 目的地までの距離
 		final int distance = 300;
@@ -47,6 +48,25 @@ public class PTra10_05 {
 		 * ★ 目的地についた時点で「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
+		int sum = 0;
+		int time = 0;
+		while(sum < distance) {
+			time++;
+
+			int move = car.run();
+
+			if(move > 0) {
+				sum += move;
+			} else {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+
+		if(sum >= distance) {
+			System.out.println("目的地までに"+time+"時間かかりました。残りのガソリンは、"+car.gasoline+"リットルです");
+		}
+
 
 	}
 }
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -16,8 +16,83 @@ public class PTra10_06 {
 
 		/*
 		 * ★ carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
-		 * ★ 各carインスタンスのrunメソッドを実行して、200km先の目的地に到達した順位を出力してください
+		 * ★ 各carインスタンスのrunメソッドを実行して、それぞれかかった時間と残りのガソリンの量を出力してください。
 		 */
+
+		Car car1 = new Car();
+		car1.serialNo = 10000;
+		car1.color = "Red";
+		car1.gasoline = 50;
+
+		Car car2 = new Car();
+		car2.serialNo = 10000;
+		car2.color = "White";
+		car2.gasoline = 40;
+
+		Car car3 = new Car();
+		car3.serialNo = 10000;
+		car3.color = "Black";
+		car3.gasoline = 80;
+
+		final int distance = 300;
+
+		int sum1 = 0;
+		int time1 = 0;
+		while(sum1 < distance) {
+			time1++;
+
+			int move = car1.run();
+
+			if(move > 0) {
+				sum1 += move;
+			} else {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+
+		if(sum1 >= distance) {
+			System.out.println("目的地までに"+time1+"時間かかりました。残りのガソリンは、"+car1.gasoline+"リットルです");
+		}
+
+		int sum2 = 0;
+		int time2 = 0;
+		while(sum2 < distance) {
+			time2++;
+
+			int move = car2.run();
+
+			if(move > 0) {
+				sum2 += move;
+			} else {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+
+		if(sum2 >= distance) {
+			System.out.println("目的地までに"+time2+"時間かかりました。残りのガソリンは、"+car2.gasoline+"リットルです");
+		}
+
+		int sum3 = 0;
+		int time3 = 0;
+		while(sum3 < distance) {
+			time3++;
+
+			int move = car3.run();
+
+			if(move > 0) {
+				sum3 += move;
+			} else {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+
+		if(sum3 >= distance) {
+			System.out.println("目的地までに"+time3+"時間かかりました。残りのガソリンは、"+car3.gasoline+"リットルです");
+		}
+
 
 	}
 }
